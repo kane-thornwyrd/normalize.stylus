a60d14f5494d80d3a365bd2ed8dde80b061641b3
Jean-Cédric Thérond <jean.cedric.t@gmail.com>
2013-12-31 11:24:33 +0100

    Adding simple express server to be able to test Normalize.Stylus within a minute

---
+++ testing/app.js
@@ -0,0 +1,47 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+var stylus = require('stylus');
+
+var app = express();
+
+
+  // all environments
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded());
+  app.use(express.methodOverride());
+  app.use(app.router);
+
+
+  app.use(
+    stylus.middleware({
+      src: path.join(__dirname, '..'),
+      dest: path.join(__dirname, 'public'),
+      linenos: true,
+      force: true
+    })
+  );
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  // development only
+  if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+  }
+
+  app.get('/', routes.index);
+
+  http.createServer(app).listen(app.get('port'), function(){
+    console.log('Listening on http://127.0.0.1:' + app.get('port'));
+  });
+
